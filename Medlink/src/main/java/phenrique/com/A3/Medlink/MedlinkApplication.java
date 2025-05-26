2dc7d8ae4cc69e1ec934e921b4b0940710118809
Pedro Freitas <103539537+Pedro-prg@users.noreply.github.com>
2025-05-26 19:24:07 -0300

    Arquivos base

---
+++ Medlink/src/main/java/phenrique/com/A3/Medlink/MedlinkApplication.java
@@ -0,0 +1,13 @@
+package phenrique.com.A3.Medlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlinkApplication.class, args);
+	}
+
+}
---
+++ Medlink/src/test/java/phenrique/com/A3/Medlink/MedlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package phenrique.com.A3.Medlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
